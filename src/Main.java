3afeba2808165887226f319a1b8127608746ce1d
Seth Simo <seths35@nycstudents.net>
2025-09-22 08:40:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;  // must import this class b/c it isn't "built in" like String and System
+
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);  // create a Scanner "object"
+
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("|Note! do not use any symbols when inputting data! (no $ or %)");
+        System.out.println("---------------------------------------------------------------");
+
+        System.out.print("Enter your Bill($): ");
+        double billPT = scan.nextDouble();
+        scan.nextLine();
+        System.out.print("Tip Percent: ");
+        double tipP = scan.nextDouble();
+        scan.nextLine();
+        System.out.print("Number of People: ");
+        double numP = scan.nextDouble();
+        scan.nextLine();
+        double tipTotal100 = (Math.round(billPT*(tipP)));
+        double tipTotal = tipTotal100/100;
+        double billTotal = tipTotal+billPT;
+        double tipPP100 = Math.round(tipTotal100/numP);
+        double tipPP = tipPP100/100;
+        double totalPP100 = Math.round(billTotal*100/numP);
+        double totalPP = totalPP100/100;
+
+
+
+
+        scan.close();
+    }
+}
